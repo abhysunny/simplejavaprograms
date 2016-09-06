af3f0a1bf437ad2d50079e5015d74b1649784f45
Abhy Sunny <abhysunny@gmail.com>
2016-09-06 20:20:02 +0530

    Updated code for Printing Array

--- src/ddjavapackage/PrintArray.java
+++ src/ddjavapackage/PrintArray.java
@@ -1,10 +1,25 @@
 package ddjavapackage;
 
-public class PrintArray {
+import java.util.Scanner;
 
+public class PrintArray {
 	public static void main(String[] args) {
     //Program to print an array of numbers
+	System.out.println("Enter the no: of elements to be printed");
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt(); 
 	
+	int[] array = new int[10];
+	
+	for(int i=0;i<n;i++){
+	System.out.println("Enter element");
+	array[i] = sc.nextInt();	
+	}
+	System.out.println("The array of numbers stored are:");
+	for(int i=0;i<n;i++){
+	System.out.println(array[i]);
+	}
+	sc.close();
 	}
 
 }
