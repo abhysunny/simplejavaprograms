edfe983e03f659e49bbce8473e9d88a27dea941c
asunnyx <asunnyx@asunnyx-MOBL.mshome.net>
2021-01-18 06:52:32 +0530

    ArrayList Sample

---
+++ src/ddjavapackage/SampleArraylist.java
@@ -0,0 +1,21 @@
+package ddjavapackage;
+
+import java.util.ArrayList;
+
+public class SampleArraylist {
+	
+	public static void main(String []args){
+		
+	
+		ArrayList<String> cars = new ArrayList<String>();
+		
+		cars.add("Tiago");
+		cars.add("Nano");
+		cars.add("Nexon");
+		
+		System.out.println("That tata cars are: " + cars);
+		
+		
+	}
+
+}
